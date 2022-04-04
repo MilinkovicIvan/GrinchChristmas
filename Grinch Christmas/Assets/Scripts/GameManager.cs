c2b604aa657870aea0b0f4d0d5640edb0c7622f5
MilinkovicIvan <ivan.milinkovic91@gmail.com>
2022-04-04 09:51:11 +0100

    Main menu UI and gameManager
    project now have UI for main menu, backgrounds, animations, play, cloud and settings buttons. Also have game manager object which will control things inside game, currently have controls for what happens when player hit play button, starts gameplay scene

---
+++ Grinch Christmas/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //gamemanager singleton
+    public static GameManager instance { get; private set; }
+
+    public void Awake() {
+        // if instance doesnt exist create it
+        if (instance == null)
+        {
+            instance = this;
+            // when scene is loaded, restarted, dont destroy the object
+            DontDestroyOnLoad(gameObject);
+        }
+        // else destroy it
+        else if (instance != null){
+            Destroy(gameObject);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
